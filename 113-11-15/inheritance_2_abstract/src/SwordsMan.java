181adac2ef05a8bbba7ae6e5aac9720479811bcc
A112223004 <a112223004@mail.shu.edu.tw>
2024-11-15 11:59:49 +0800

    113-11-15

---
+++ 113-11-15/inheritance_2_abstract/src/SwordsMan.java
@@ -0,0 +1,8 @@
+public class SwordsMan extends Role{
+        public void fight() {
+            System.out.println("揮劍攻擊");
+        }
+
+
+}
+
---
+++ 113-11-15/inheritance_3_overwiting/src/SwordsMan.java
@@ -0,0 +1,10 @@
+public class SwordsMan extends Role {
+    @Override
+    public void fight() {
+            super.fight();
+            System.out.println("揮劍攻擊");
+        }
+
+
+}
+
