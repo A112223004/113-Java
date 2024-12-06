524dcbf85fffed076ac67079aecaacb39a1c1ebc
A112223004 <a112223004@mail.shu.edu.tw>
2024-12-06 12:04:59 +0800

    113/12/06

---
+++ 113-12-06/TryCash/src/UncheckedExecption.java
@@ -0,0 +1,11 @@
+public class UncheckedExecption {
+    public static void main(String[] args){
+        int[] numbers = {1, 2, 3};
+
+        try {
+            System.out.println(numbers[5]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("處理未檢例外: " + e.getMessage());
+        }
+    }
+}
